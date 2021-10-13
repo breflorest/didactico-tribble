7df7d557777bcb8455972cc54030c7ea864e329a
breflorest <92409648+breflorest@users.noreply.github.com>
2021-10-12 21:13:52 -0500

    Test- Programa de Control de Bucles: Continue
    Control de Bucles

---
+++ Continue.py
@@ -0,0 +1,23 @@
+autos = ["correcto", "correcto", "correcto", "defectuoso", "correcto", "defectuoso", "correcto"]
+
+for estado in autos:
+  if estado == "defectuoso":
+    print("Auto defectuoso, enviado a revicion")
+    continue
+  print("Este auto esta: " + estado)
+  print("Enviado para distribucion")
+
+
+  
+  
+  
+ 
+for caracter in "Python":
+
+  if caracter == "h":
+
+    continue
+
+    print ( "Esta es una h" )
+
+  print ( caracter )
